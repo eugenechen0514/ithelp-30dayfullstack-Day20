95afb3bf95be315ca073ba76fe80fe00656398c6
Unknown <yuchiehchen@outlook.com>
2018-10-20 16:10:54 +0800

    處理 SECRET 相依：SECRET 改成外界送入

--- middlewares/VerifyJWT.js
+++ middlewares/VerifyJWT.js
@@ -3,20 +3,21 @@ const jsonwebtoken = require('jsonwebtoken');
 
 const SECRET = 'YOUR_JWT_SECRET'; // 要合簽發時一樣，所以可以放在 ./configs/config.js 中
 
-async function verifyJWT(jwt) {
+async function verifyJWT(jwt, {secret}) {
     if (!jwt) {
         return Promise.reject(new Error('No JWT'));
     }
-    const decoded = jsonwebtoken.verify(jwt, SECRET);
+    const decoded = jsonwebtoken.verify(jwt, secret);
     return decoded;
 }
 
 
 module.exports = function (options = {}) {
-    const {tokenPath = 'cookies.token'} = options; // tokenPath 是取出 token 的路徑
+    // tokenPath 是取出 token 的路徑
+    const {tokenPath = 'cookies.token', secret = SECRET} = options; 
     return function (req, res, next) {
         const jwt = _.get(req, tokenPath);
-        verifyJWT(jwt)
+        verifyJWT(jwt, {secret})
             .then(decoded => {
                 console.log(decoded);
                 next(); // next middleware

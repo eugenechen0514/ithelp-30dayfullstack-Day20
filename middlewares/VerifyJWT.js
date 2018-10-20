323dcdde57680cf31f82d6ad5f267caaeff75877
Unknown <yuchiehchen@outlook.com>
2018-10-20 16:14:15 +0800

    處理 jsonwebtoken 相依：jsonwebtoken.verify 改成外界送入

--- middlewares/VerifyJWT.js
+++ middlewares/VerifyJWT.js
@@ -3,21 +3,21 @@ const jsonwebtoken = require('jsonwebtoken');
 
 const SECRET = 'YOUR_JWT_SECRET'; // 要合簽發時一樣，所以可以放在 ./configs/config.js 中
 
-async function verifyJWT(jwt, {secret}) {
+async function verifyJWT(jwt, {secret, verify}) {
     if (!jwt) {
         return Promise.reject(new Error('No JWT'));
     }
-    const decoded = jsonwebtoken.verify(jwt, secret);
+    const decoded = verify(jwt, secret);
     return decoded;
 }
 
 
 module.exports = function (options = {}) {
     // tokenPath 是取出 token 的路徑
-    const {tokenPath = 'cookies.token', secret = SECRET} = options; 
+    const {tokenPath = 'cookies.token', secret = SECRET, verify = jsonwebtoken.verify} = options;
     return function (req, res, next) {
         const jwt = _.get(req, tokenPath);
-        verifyJWT(jwt, {secret})
+        verifyJWT(jwt, {secret, verify})
             .then(decoded => {
                 console.log(decoded);
                 next(); // next middleware
